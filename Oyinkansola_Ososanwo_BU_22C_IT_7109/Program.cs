9dc54caa7e8e9b3c0f104aa9ded07b801d28748d
oyinsquo <122940264+oyinsquo@users.noreply.github.com>
2024-06-28 23:17:20 +0100

    UPDATE

---
+++ Oyinkansola_Ososanwo_BU_22C_IT_7109/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string response;
+console.WriteLine("Whats your name?");
+response = Console.Readline();
+Console.WriteLine($"Enjoy the course, {response}");
+
+OperatingSystem thisOS = Envirinment.OSVersion;
+Console.WriteLine(thisOS.Platform);
+Console.WriteLine(thisOS.VersionString);
